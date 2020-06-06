d642e5a8c799cf41fe8c5f2c5d17b100e6c809f7
Stefan Wachter <stefan.wachter@gmx.de>
2020-06-06 11:57:24 +0200

    add function test

--- e2e/src/main/scala/e2e/file1.scala
+++ e2e/src/main/scala/e2e/file1.scala
@@ -88,11 +88,9 @@ class JsClass(val initialStr: String, var num: Int) extends js.Object {
 
 object FunctionTest {
   @JSExportTopLevel("fun0")
-  def fun0[R](f: js.Function0[R]): R = f.apply()
+  def fun0[R](f: js.Function0[R]): R = f()
   @JSExportTopLevel("fun1")
-  def fun1[T1, R](f: js.Function1[T1, R]) = ()
+  def fun1[T1, R](a: js.Array[T1], f: js.Function1[T1, js.Array[R]]): js.Array[R] = a.flatMap(f)
   @JSExportTopLevel("fun2")
-  def fun2[T1, T2, R](f: js.Function2[T1, T2, R]) = ()
-  @JSExportTopLevel("fun3")
-  def fun3[T1, R](f: js.Function2[js.Function0[R], js.Function1[T1, R], R]) = ()
+  def fun2[T1, T2, R](a1: js.Array[T1], a2: js.Array[T2], f: js.Function2[T1, T2, R]): js.Array[R] = a1.zip(a2).map(f.tupled)
 }
