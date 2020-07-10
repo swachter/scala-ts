cb98cd8460c08aa6b686d5714da7a52242723353
Stefan Wachter <stefan.wachter@gmx.de>
2020-07-10 18:26:51 +0200

    add example cross project that uses endpoints4s

---
+++ example/angular/client/src/main/scala/CounterClient.scala
@@ -0,0 +1,34 @@
+package org.test.client
+
+import endpoints4s.xhr
+import org.scalajs.dom.XMLHttpRequest
+import org.test.shared.{Counter, CounterEndpoints, Increment}
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSExportTopLevel
+
+/**
+ * Defines an HTTP client for the endpoints described in the `CounterEndpoints` trait.
+ * The derived HTTP client uses XMLHttpRequest to perform requests and returns
+ * results in a `js.Thenable`.
+ */
+@JSExportTopLevel("CounterClient")
+object CounterClient extends js.Object {
+
+  private object client
+    extends CounterEndpoints
+    with xhr.thenable.Endpoints
+    with xhr.JsonEntitiesFromSchemas
+
+  /**
+   * Performs an XMLHttpRequest on the `currentValue` endpoint, and then
+   * deserializes the JSON response as a `Counter`.
+   */
+  val eventuallyCounter: js.Thenable[Counter] = client.currentValue(())
+
+  /**
+   * Serializes the `Increment` value into JSON and performs an XMLHttpRequest
+   * on the `increment` endpoint.
+   */
+  val eventuallyDone: js.Thenable[Unit] = client.increment(Increment(42))
+}
