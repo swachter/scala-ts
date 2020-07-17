b872c7dd01828e520fb761959a7b8d1d4c109563
Stefan Wachter <stefan.wachter@gmx.de>
2020-07-17 14:11:01 +0200

    add test for unexported members (class + object)

---
+++ generator/src/test/scala/eu/swdev/scala/ts/dts/SealedTraitUnexportedMembersTest.scala
@@ -0,0 +1,58 @@
+package dts
+
+import eu.swdev.scala.ts.DtsFunSuite
+
+import scala.scalajs.js.annotation.JSExportTopLevel
+
+class SealedTraitUnexportedMembersTest extends DtsFunSuite {
+
+  """
+    |export interface Case1 extends dts.SealedTraitUnexportedMembersTest$.Base {
+    |  'Case1': never
+    |}
+    |export class Case1 {
+    |  constructor()
+    |}
+    |export namespace dts {
+    |  namespace SealedTraitUnexportedMembersTest$ {
+    |    interface Base {
+    |      'dts.SealedTraitUnexportedMembersTest$.Base': never
+    |    }
+    |    type Base$u = Case1 | dts.SealedTraitUnexportedMembersTest$.Case2 | dts.SealedTraitUnexportedMembersTest$.Case3$
+    |    interface Case2 extends dts.SealedTraitUnexportedMembersTest$.Base {
+    |      'dts.SealedTraitUnexportedMembersTest$.Case2': never
+    |    }
+    |    interface Case3$ extends dts.SealedTraitUnexportedMembersTest$.Base {
+    |      'dts.SealedTraitUnexportedMembersTest$.Case3$': never
+    |    }
+    |  }
+    |}
+    |""".check()
+
+}
+
+object SealedTraitUnexportedMembersTest {
+
+  sealed trait Base
+
+  @JSExportTopLevel("Case1")
+  class Case1 extends Base
+
+  class Case2 extends Base
+
+  object Case3 extends Base
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
